37f925347dd4ac96d2cea1a6378e53bae444eb40
hayesejasmine <hayesejasmine@gmail.com>
2023-03-13 02:21:56 -0400

    resume added

---
+++ src/components/pages/resume.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+const styles = {
+
+};
+
+export default function Resume(){
+    <div style={styles.space}>
+        <h2 style={styles.h2}>Resume</h2>
+        <br></br>
+        <h3 style={styles.h3}>Technical Skills</h3>
+        <p><h4>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>JQuery</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+                <li>MySQL</li>
+                <li>Express</li>
+                <li>Node</li>
+                <li>APIs</li>
+                <li>Sequelize</li>
+            </ul>
+            </h4></p>
+            <br></br>
+
+<h3 style={styles.h3}>Education</h3>
+<p>
+<ul>
+    <li>University of North Carolina Charlotte- Charlotte, NC (2022-2023)- Coding Bootcamp Certificate of Completion</li>
+    <li>East Carolina University- Greenville, NC (2016-2018)- Master of Public Health Concentration- Health Administration</li>
+    <li>East Carolina University- Greenville, NC (2010-2014)- Bachelor of Science in Public Health Concentration- Community Health</li>
+</ul>
+</p>
+
+
+            <h3 style={styles.h3}>Work Experience</h3>
+            <p>
+                <h4>Rocket Mortgage- Charlotte, NC (May 2019-present)</h4>
+                <h5>Change in Circumstance Underwriter</h5>
+                <ul>
+                    <li>● Receive and process requested loan changes made by third party loan officer/loan processors</li>
+                    <li>● Communicate and advise partners of product guidelines via phone and email</li>
+                    <li>● De-escalate request escalations made by loan officers</li>
+                    <li>● Communicate with and redirect loan structure changes to the appropriate internal teams</li>
+                    <li>● Maintain knowledge on product and company compliance guidelines by attending trainings and utilizing provided resources</li>
+                    
+                </ul>
+            </p>
+
+            <p>
+                <h4>East Carolina University- Department of Criminal Justice-The Office of Violence Against Women Greenville, NC (Aug. 2018-Dec. 2018)</h4>
+                <h5>Graduate Assistant</h5>
+                <ul>
+                <li>● Analyzed qualitative data from court files to actively decipher material to be entered into an access database</li>
+                <li>● Corresponded with victim advocates in the form of interviews to compose information for the innovation of domestic violence homicide reduction and prevention models</li>
+                    
+                </ul>
+            </p>
+
+            <p>
+                <h4>Plato’s Closet- Greenville, NC (Oct. 2013 – Dec 2018)</h4>
+                <h5>Key Holder/ Social Media Assistant</h5>
+                <ul>
+                <li>● Opened and closed the store according to schedule, while balancing the opening and closing tills as needed</li>
+                <li>● Became familiar with products, brands and prices for the fulfillment of making product recommendations</li>
+                <li>● Maintained store displays and social media platforms to promote store products for the increase of sales</li>
+                <li>● Achieved personal and store goals by applying sound customer service, sales protocol and the supervision and coordination of other staff, as directed by Management</li>
+                    
+                </ul>
+            </p>
+
+            <p>
+                <h4>North Carolina Council for Women & Youth Involvement, Eastern Region- New Bern, NC (May 2018- July 2018)</h4>
+                <h5>Intern</h5>
+                <ul>
+                <li>● Constructed a survey to determine existing transportation and language resources for the integration of expanding language and transportation access among sheltered survivors</li>
+                <li>● Analyzed survey results and authored informational material to disseminate findings to stakeholders</li>
+                    
+                </ul>
+            </p>
+
+            <p>
+                <h4>East Carolina University -Department of Public Health- Greenville, NC (Sept. 2016- May 2018)</h4>
+                <h5>Student Researcher</h5>
+                <ul>
+                <li>● Drafted, edited, finalized, and performed pilot tests on data collection materials to be used during the implementation of the program</li>
+                <li>● Corresponded with local media outlets and attended outreach programs to share the purpose and development of the project</li>
+                <li>● Facilitated focus group discussions and key informant interviews</li>
+                <li>● Centralized key findings by transcribing audio recordings, deciphering written notes and organizing participant demographics for the use of laying the groundwork for designing a community-based participatory research model</li>
+
+                    
+                </ul>
+            </p>
+
+            <p>
+                <h4>Pasquotank County Department of Social Services- Elizabeth City, NC (April 2015- August 2016)</h4>
+                <h5>Income Maintenance Caseworker II</h5>
+                <ul>
+                <li>● Determined eligibility for existing Medicaid clients through semi-structured client interviews and operating verification sites to query client qualification factors</li>
+                <li>● Managed a Caseload on 40-60 files per month</li>
+                <li>● Systematized client's case files, ensuring that all data entries were up to date to comply with auditing regulations</li>
+                    
+                </ul>
+            </p>
+    
+
+<br></br>
+
+<h3 style={styles.h3}>Other Skills</h3>
+<p>
+<ul>
+<li>Computer- HTML, CSS, Javascript, Microsoft Office (Word, Excel, Outlook, PowerPoint), SAS, SPSS, Salesforce, Cherwell, Database Management, Adobe Suite</li>
+<li>Personal- Cultural competency, ability to develop and maintain effective working relationships, Strong organizational skills, strong communication skills, strong administrative skills, program planning and implementation, monitoring and evaluation, quick learner, strong time management skills</li>
+</ul>
+</p>
+</div>
+}
