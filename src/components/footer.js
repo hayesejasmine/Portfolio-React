b41080ead1715362a9760a52726eee1124b59d47
hayesejasmine <hayesejasmine@gmail.com>
2023-03-12 22:52:45 -0400

    images added and footer updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,17 +9,21 @@ function Footer() {
 <footer className="text-center">
 <div className="footer">
     <section>
-    
-            <a href="mailto:hayesejasmine@gmail.com" target="blank"><img stye={styles.icons} src={email} alt="email icon"/> </a>
-                       
-            <a href="https://github.com/hayesejasmine" target="blank"><img stye={styles.icons} src={Github} alt="Github icon"/> </a>
-                        
-            <a href="https://www.linkedin.com/in/jasmine-hayes-mph-8a39ab178/" target="blank"><img stye={styles.icons} src={LinkedIn} alt="Linkedin icon"/> </a>
-                        
-            <p>Phone: (704) 606-9254</p>
-                       
+    <ul>
+                        <li>
+                            <a href="mailto:hayesejasmine@gmail.com">Email</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/hayesejasmine" target="_blank">GitHub</a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/jasmine-hayes-mph-8a39ab178/" target="_blank">LinkedIn</a>
+                        </li>
+                        <li>
+                            <p>Phone: (704) 606-9254</p>
+                        </li>
+                    </ul>                  
     </section>
-
 </div>
 </footer>
     );
