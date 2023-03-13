804927121bd7ea9f3938c74a27632102e8de5c1c
hayesejasmine <hayesejasmine@gmail.com>
2023-03-12 23:30:59 -0400

    about page updated

---
+++ src/components/pages/about.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const styles = {
+
+}
+
+export default function About() {
+    return (
+<div style={styles.space}>
+    <h2 style={styles.h2}>About Me</h2>
+<div>
+<img style={styles.portfolio2} src={portfolio2} alt="professional headshot" />
+</div>
+<p>
+Hello, welcome to my page! 
+                     
+<br> I am a fullstack web developer student located in Charlotte, NC. I am currently enrolled in the Coding Boot Camp at UNC Charlotte. I love structure, order and most of all, quality. I am excited to continue refining my skills through assisting others in bringing their web ideas to life!     
+</br>
+<br></br>
+</p>
+</div>
+    );
+}
