234373e44e63aa4fca148a7ca7f8990305bf4646
hayesejasmine <hayesejasmine@gmail.com>
2023-03-13 01:37:12 -0400

    portfolio page updated

---
+++ src/components/pages/contact.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+
+import { validateEmail} from "../../utils/helpers";
+
+const styles = {
+
+};
+
+function contactForm() {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const handleInputChange = (e) => {
+        const { target } e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === "email") {
+            setEmail(inputValue);
+        } else if (inputType === "name") {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(!validateEmail(email)) {
+            alert("Email unsuccessful");
+            return;
+        }
+
+        if (!name) {
+            alert("This field is required");
+            return;
+        }
+
+        if (!message) {
+            alert("This field is required");
+        }
+
+        if (email || name || message) {
+            setSuccess("Thank you for your interest. I look forward to speaking with you soon!");
+            return;
+        }
+
+        setName("");
+        setEmail("");
+        setMessage("");
+    };
+
+    return (
+        <div>
+        <h2 style={styles.h2}>Contact</h2>
+        <div style={styles.email}>
+            <a href="mailto:hayesejasmine@gmail.com">hayesejasmine@gmail.com</a>
+            </div>
+            <form action="mailto:hayesejasmine@gmail.com"> method= "post" encType="txt/plain" style={styles.form} className="form"</form>"
+            <div style={styles.input}>
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+            required
+          />
+        </div>
+        <div style={styles.input}>
+          <input
+            value={Name}
+            name="Name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="name"
+            required
+          />
+        </div>
+        <div style={styles.input}>
+          <input
+            style={styles.inputa}
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="message"
+            required
+          />
+        </div>
+        <div>
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </div>
+     </form>
+     {setSuccess && (
+        <div style={styles.form}>
+        <p className="success-text">{setSuccess}</p>
+        </div>
+     )}
+</div>
+    );
+}
+
+export default contactForm;
---
+++ src/components/pages/portfolio.js
@@ -0,0 +1,65 @@
+import React from "react";
+import PasswordGenerator from "./assets/img/javascript-password generator copy.png";
+import CodeRefactor from "./assets/img/Image 10-23-22 at 4.47 PM copy.jpg";
+import ServerSideAPIProject from "./assets/img/Server side API copy.png";
+import ThirdPartyAPIScheduler from "./assets/img/Third-Party APIs  Work Day Scheduler copy.png";
+import webAPICodingQuiz from "./assets/img/web-api-coding quiz copy.png";
+
+const styles = {
+
+};
+
+export default function Portfolio() {
+    return (
+<div>
+    <h2 style={styles.h2}>Portfolio</h2>
+    <section class="my-work">
+<div className="my-work">
+<div className= "projects-grid-wrapper">
+<div className="projects-grid">
+
+    <a href="https://hayesejasmine.github.io/Accessibility-Refactor-Challenge/" target="_blank" class="project refactoring" src={CodeRefactor}>
+        <div class="project-label">
+            <h3>Code Refactoring</h3>
+            <p>HTML/CSS</p>
+        </div>
+    </a>
+    <a href="https://hayesejasmine.github.io/password-generate/index.html" target="_blank" class="project project-generator" src={PasswordGenerator}>
+        <div class="project-label">
+            <h3>"Password Generator Quiz"</h3>
+            <p>JavaScript</p>
+        </div>
+    </a>
+    <a href="https://hayesejasmine.github.io/Coding-Quiz/index.html" target="_blank" class="project coding-quiz" src={webAPICodingQuiz}>
+        <div class="project-label">
+            <h3>Coding Quiz</h3>
+            <p>HTML/CSS/JavaScript</p>
+        </div>
+    </a>
+    <a href="https://hayesejasmine.github.io/Workday-Calender/Develop/index.html" target="_blank"  class="project planner" src={ThirdPartyAPIScheduler}>
+        <div class="project-label">
+            <h3>Workday Schdeule Planner"</h3>
+            <p>Third-Party APIs</p>
+        </div>
+    </a>
+    <a href="https://beefbones.github.io/travel-planner/" target="_blank" class="project 1 travel-planner" src={ServerSideAPIProject}>
+        <div class="project-label">
+            <h3>"Travel Planner Site"</h3>
+            <p>Server Side APIs</p>
+            <p>Project 1</p>
+        </div>
+    </a>
+    <a href="https://music-review-4-u.herokuapp.com/login" target="_blank" class="project 2 mvc">
+        <div class="project-label">
+            <h3>"Review4U"</h3>
+            <p>MVC</p>
+            <p>Project 2</p>
+        </div>
+    </a>
+</div>
+</div>
+</div>
+</section>
+</div>
+    );
+}
