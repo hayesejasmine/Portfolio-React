78eb75f40f1e733fae7a9ab54cd2a78563460ad9
hayesejasmine <hayesejasmine@gmail.com>
2023-03-16 03:58:00 -0400

    updated contact page and media query

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -7,103 +7,38 @@ const styles = {
 };
 
 function ContactForm() {
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [message, setMessage] = useState("");
-    const [success, setSuccess] = useState("");
-
-    const handleInputChange = (e) => {
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
-
-        if (inputType === "email") {
-            setEmail(inputValue);
-        } else if (inputType === "name") {
-            setName(inputValue);
-        } else {
-            setMessage(inputValue);
-        }
-    };
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-
-        if(!validateEmail(email)) {
-            alert("Email unsuccessful");
-            return;
-        }
-
-        if (!name) {
-            alert("This field is required");
-            return;
-        }
-
-        if (!message) {
-            alert("This field is required");
-        }
-
-        if (email || name || message) {
-            setSuccess("Thank you for your interest. I look forward to speaking with you soon!");
-            return;
-        }
-
-        setName("");
-        setEmail("");
-        setMessage("");
-    };
-
-    return (
-        <div id="contact">
+  return (
+    <div id="contact">
         <h2 style={styles.h2}>Contact Me</h2>
-        <div  style={styles.email}>
-            <a href="mailto:hayesejasmine@gmail.com"></a>
-            </div>
+        <div className="contact-form">
             <form action="mailto:hayesejasmine@gmail.com" method= "post" encType="txt/plain" style={styles.form} className="form">
-            <div style={styles.input} className="email">
-          <input
-            value={email}
-            name="email"
-            onChange={handleInputChange}
-            type="email"
-            placeholder="Email"
-            required
-          />
+            <div style={styles.input} className="formgroup">
+          <label for="email"></label>
+           <textarea className="email" id="email" placeholder="Email"></textarea>
         </div>
-        <div style={styles.input} className="name">
-          <input
-            value={name}
-            name="Name"
-            onChange={handleInputChange}
-            type="text"
-            placeholder="Full Name"
-            required
-          />
+        <div style={styles.input} className="formgroup">
+          <label for="name"></label>
+           <textarea className="name" id="name" placeholder="Name"></textarea>
         </div>
-        <div style={styles.input} className="message">
-          <input
-            style={styles.inputa}
-            value={message}
-            name="message"
-            onChange={handleInputChange}
-            type="message"
-            placeholder="Message"
-            required
-          />
+        <div style={styles.input} className="formgroup">
+          <label for="message"></label>
+           <textarea className="message" id="message" placeholder="Send me a message"></textarea>
         </div>
-        <div>
-          <button type="button" onClick={handleFormSubmit}>
-            Submit
-          </button>
+        <div style={styles.input} className="formgroup">
+          <button className="form-button" type="button">Submit</button>
+         
         </div>
-     </form>
-     {setSuccess && (
-        <div style={styles.form}>
-        <p className="success-text">{setSuccess}</p>
+        </form>
         </div>
-     )}
-</div>
-    );
-}
+        </div>
+  );
+
+  }
 
 export default ContactForm;
+
+
+
+
+
+
