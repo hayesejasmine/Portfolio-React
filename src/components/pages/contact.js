9e234b661c4dabc18efacf82adc6c85712aa5560
hayesejasmine <hayesejasmine@gmail.com>
2023-03-16 04:17:50 -0400

    removed utils folder

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import { validateEmail} from "../../utils/helpers";
 
 const styles = {
 
--- src/utils/helpers.js
+++
@@ -1,4 +0,0 @@
-export function validateEmail(email) {
-    const re=/^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i;
-    return (email);
-}
